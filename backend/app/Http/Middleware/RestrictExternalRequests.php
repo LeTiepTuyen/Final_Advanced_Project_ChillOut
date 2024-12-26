8e98fec5deca2745eec2a88a3394f3c5ecc2eebd
Way-curious <tuyentieple@gmail.com>
2024-12-26 14:50:58 +0700

    Setup Sentry for error monitoring

--- backend/app/Http/Middleware/RestrictExternalRequests.php
+++ backend/app/Http/Middleware/RestrictExternalRequests.php
@@ -7,24 +7,20 @@ use Illuminate\Http\Request;
 
 class RestrictExternalRequests
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
-     * @return mixed
-     */
     public function handle(Request $request, Closure $next)
     {
         $trustedDomains = ['localhost:3000', 'localhost:8000'];
-        $origin = parse_url($request->headers->get('origin'), PHP_URL_HOST) . ':' . parse_url($request->headers->get('origin'), PHP_URL_PORT);
+        $originHeader = $request->headers->get('origin');
 
-        // Allow trusted domains
-        if (in_array($origin, $trustedDomains)) {
+        if ($originHeader) {
+            $origin = parse_url($originHeader, PHP_URL_HOST) . ':' . parse_url($originHeader, PHP_URL_PORT);
+            if (in_array($origin, $trustedDomains)) {
+                return $next($request);
+            }
+        } else {
             return $next($request);
         }
 
-        // Check for Bearer token for other domains
         if ($request->bearerToken()) {
             return $next($request);
         }
